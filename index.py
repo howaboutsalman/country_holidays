b5a1272e8a7b5f563851fcf2c6c7329e0e8cb03c
Salman <78206653+samofxi@users.noreply.github.com>
2022-10-31 16:34:39 +0100

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from datetime import date
 
 import holidays
 varCountry = input('inter a Country: ')
+varYear = input('inter a Year: ')
 
-for ptr in holidays.country_holidays(varCountry, years=2028).items():
+for ptr in holidays.country_holidays(varCountry, years=varYear).items():
     print(ptr)
